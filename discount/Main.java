6c8ecb8f8eea9764f014400ffb1831010686e7bf
Andrew Oluoch <oluoch3@gmail.com>
2020-03-12 09:36:45 -0400

    pusher man

---
+++ discount/Main.java
@@ -0,0 +1,19 @@
+package discount;
+
+public class Main {
+
+    public static void discount(float markedprice, float discountedprice) {
+        System.out.println(discountedprice/100*markedprice);
+
+
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        discount(200,10);
+
+    }
+}
+
